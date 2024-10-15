ecf1e887aa3d99147a18e5c77e2f8ca78eab5b0d
adamjhr <adam.jh.rose6@gmail.com>
2024-10-15 12:07:30 +0200

    Metadata structs and reading larger chunks of data
    The initial structs and code for reading and sending ring buffer metadata from server to client.
    Furthermore, the dtp_server now reads one chunk of payload data at a time, instead of the entire file

--- src/include/vmem/vmem_dtp_server.h
+++ src/include/vmem/vmem_dtp_server.h
@@ -1,4 +1,18 @@
 #ifndef DIPP_DTP_SERVER_H
 #define DIPP_DTP_SERVER_H
 
+#include "metadata.pb-c.h"
+
+typedef struct RingBufferElementMetadata {
+    uint32_t index;
+    uint32_t metadata_len;
+    Metadata metadata;
+} RingBufferElementMetadata;
+
+typedef struct RingBufferMetadata {
+    uint32_t size;
+    uint32_t elements_len;
+    RingBufferElementMetadata *elements;
+} RingBufferMetadata;
+
 #endif
--- src/vmem/vmem_dtp_server.c
+++ src/vmem/vmem_dtp_server.c
@@ -2,9 +2,35 @@
 #include <dtp/platform.h>
 #include <vmem/vmem.h>
 #include <vmem/vmem_ring.h>
+#include "vmem_ring_buffer.h"
+#include "protobuf/metadata.pb-c.h"
 
-static uint32_t element_size(vmem_t * vmem, uint32_t offset) {
-    vmem_ring_driver_t * driver = (vmem_ring_driver_t *)vmem->driver;
+// For these static methods, consider moving them into libparam, having vmem reference as parameter
+// Look at the naming, (offset, index, payload_id) (payload, element), be more consistent...
+
+// Calculate the offset of a ring buffer element within the ring buffer based on the payload id
+// Doesn't take metadata at start of ring buffer into account
+static uint32_t ring_buffer_offset(uint32_t payload_id, uint32_t offset) {
+    
+    vmem_ring_driver_t * driver = (vmem_ring_driver_t *)(&vmem_images)->driver;
+
+    uint32_t head = driver->head;
+    uint32_t tail = driver->tail;
+    uint32_t * offsets = (uint32_t *)driver->offsets;
+
+    uint32_t ring_buffer_index = payload_id < 0 // supports negative indexing from head
+        ? (head + payload_id + driver->entries) % driver->entries
+        : (tail + payload_id) % driver->entries;
+
+    uint32_t offset_of_payload = offsets[ring_buffer_index];
+
+    return offset_of_payload + offset;
+
+}
+
+// Find the size of a ring buffer element based on the payload_id
+static uint32_t element_size(uint32_t offset) {
+    vmem_ring_driver_t * driver = (vmem_ring_driver_t *)(&vmem_images)->driver;
 
     uint32_t head = driver->head;
     uint32_t tail = driver->tail;
@@ -32,16 +58,17 @@ static uint32_t element_size(vmem_t * vmem, uint32_t offset) {
     return len;
 }
 
-static int is_valid_index(vmem_t * vmem, uint32_t offset) {
-    vmem_ring_driver_t * driver = (vmem_ring_driver_t *)vmem->driver;
+// Check if the payload_id provided results in a valid ring buffer index
+static int is_valid_index(uint32_t payload_id) {
+    vmem_ring_driver_t * driver = (vmem_ring_driver_t *)(&vmem_images)->driver;
 
-    if (offset >= driver->entries) return 0;
+    if (payload_id >= driver->entries) return 0;
 
     uint32_t head = driver->head;
     uint32_t tail = driver->tail;
     uint32_t * offsets = (uint32_t *)driver->offsets;
 
-    int offset_int = (int)offset;
+    int offset_int = (int)payload_id;
 
     uint32_t index = offset_int < 0
         ? (head + offset_int + driver->entries) % driver->entries
@@ -57,44 +84,22 @@ static int is_valid_index(vmem_t * vmem, uint32_t offset) {
 }
 
 static uint32_t payload_read(uint8_t payload_id, uint32_t offset, void *output, uint32_t size) {
+    uint32_t res = 0;
 
-    vmem_t *ring = NULL;
-		for(vmem_t * vmem = (vmem_t *) &__start_vmem; vmem < (vmem_t *) &__stop_vmem; vmem++) {
-			if (strncmp("images", vmem->name, 7) == 0) {
-				ring = vmem;
-				break;
-			}
-		}
-	if (ring == NULL) return;
-
-	vmem_ring_driver_t * driver = (vmem_ring_driver_t *)ring->driver;
+	vmem_ring_driver_t * driver = (vmem_ring_driver_t *)(&vmem_images)->driver;
     
-	ring->read(ring, 0, output, payload_id);
+    uint32_t ring_offset = ring_buffer_offset(payload_id, offset);
 
-    printf("Read to buffer: %s \n", output);
+	(&vmem_images)->read(&vmem_images, ring_offset, output, size);
 
-    return size;
+    return res;
 }
 
 /* This method is implemented to tell the DTP server which payload to use */
 bool get_payload_meta(dftp_payload_meta_t *meta, uint8_t payload_id) {
-    bool result = true;
-
 	printf("call to get_payload_meta");
 
-    printf("attempting to find images ring_buffer \n");
-    vmem_t *ring = NULL;
-		for(vmem_t * vmem = (vmem_t *) &__start_vmem; vmem < (vmem_t *) &__stop_vmem; vmem++) {
-			if (strncmp("images", vmem->name, 7) == 0) {
-				ring = vmem;
-				break;
-			}
-		}
-	if (ring == NULL) return;
-
-    printf("found images ring_buffer \n");
-
-    vmem_ring_driver_t * driver = (vmem_ring_driver_t *)ring->driver;
+    vmem_ring_driver_t * driver = (vmem_ring_driver_t *)(&vmem_images)->driver;
 
     uint32_t head = driver->head;
     uint32_t tail = driver->tail;
@@ -105,12 +110,12 @@ bool get_payload_meta(dftp_payload_meta_t *meta, uint8_t payload_id) {
     printf("number of entries is %u \n", driver->entries);
     printf("tail is at %u \n", tail);
     printf("head is at %u \n", head);
-    int is_valid = is_valid_index(ring, payload_id);
+    int is_valid = is_valid_index(payload_id);
     if (!is_valid) {
         printf("index out of ring buffer bounds \n");
         return false;
     }
-    uint32_t data_len = element_size(ring, payload_id);
+    uint32_t data_len = element_size(payload_id);
     printf("length of payload element is %u \n", data_len);
 
     printf("offsets are: \n");
@@ -121,5 +126,47 @@ bool get_payload_meta(dftp_payload_meta_t *meta, uint8_t payload_id) {
     meta->size = data_len;
     meta->read = payload_read;
 
-    return result;
+    return true;
 }
+
+// RingBufferElementMetadata *ring_buffer_element_info(uint32_t offset) {
+
+//     uint32_t element_len = element_size(offset);
+//     char *output = malloc(element_size); // this should perhaps be allocated statically instead
+
+//     printf("Reading element of size %u \n", element_len);
+
+//     ring->read(vmem_images, 0, output, payload_id);
+
+//     printf("Read element \n");
+
+//     uint32_t meta_size = *((uint32_t *)output);
+
+//     printf("metadata size is %u \n", meta_size);
+
+//     // In general, there is some memory stuff im unsure about, someone needs to look at this code...
+
+//     Metadata *metadata = metadata__unpack(NULL, meta_size, output + sizeof(uint32_t));
+//     struct RingBufferElementMetadata *element_metadata = malloc(RingBufferElementMetadata);
+//     element_metadata->offset;
+//     element_metadata->metadata_len = meta_size;
+//     element_metadata->metadata = metadata;
+//     return element_metadata;
+// }
+
+// RingBufferMetadata *ring_buffer_info() {
+
+//     vmem_ring_driver_t * driver = (vmem_ring_driver_t *)vmem_images->driver;
+
+//     struct RingBufferMetadata *ring_buffer_metadata = malloc(RingBufferMetadata);
+
+//     // WIP
+
+//     int index = driver->tail;
+//     while (index != head) {
+//         index = (index + 1) % driver->entries;
+//     }
+
+
+    
+// }

d70c18c83c108de7bc10a78b29c7b4fb8e964aed
adamjhr <adam.jh.rose6@gmail.com>
2024-12-01 12:26:58 +0100

    Fetching ring buffer observations and ring buffer metadata

--- src/vmem/vmem_dtp_server.c
+++ src/vmem/vmem_dtp_server.c
@@ -1,53 +1,19 @@
+#include "vmem_dtp_server.h"
 #include <dtp/dtp.h>
 #include <dtp/platform.h>
 #include <vmem/vmem.h>
 #include <vmem/vmem_ring.h>
 #include "vmem_ring_buffer.h"
-#include "protobuf/metadata.pb-c.h"
 #include <stdio.h>
-
-
-// For uploading local file into the ring buffer- TEMPORARY, FOR TESTING PURPOSES
-int upload_to_ring_buffer(char *filename) {
-
-    FILE *file = fopen(filename, "rb");
-    if (file == NULL) {
-        printf("Could not open %s\n", filename);
-        return -1;
-    }
-
-    printf("Opened file %s\n", filename);
-
-    fseek(file, 0, SEEK_END);
-    long fsize = ftell(file);
-
-    printf("filesize is %i\n", fsize);
-
-    fseek(file, 0, SEEK_SET); 
-
-    char *buffer = malloc(fsize + 1);
-    fread(buffer, fsize, 1, file);
-    fclose(file);
-
-    buffer[fsize] = 0;
-    // Thank you https://stackoverflow.com/questions/14002954/c-how-to-read-an-entire-file-into-a-buffer
-
-    (&vmem_images)->write(&vmem_images, 0, buffer, fsize);
-
-    printf("Wrote to buffer\n");
-
-    free(buffer);
-
-    return 0;
-}
-
+#include <stdlib.h>
+#include <csp/csp.h>
+#include "metadata.pb-c.h"
+#include "dtpmetadata.pb-c.h"
 
 // For these static methods, consider moving them into libparam, having vmem reference as parameter
-// Look at the naming, (offset, index, payload_id) (payload, element), be more consistent...
 
-// Calculate the offset of a ring buffer element within the ring buffer based on the payload id
-// Doesn't take metadata at start of ring buffer into account
-static uint32_t ring_buffer_offset(uint32_t payload_id, uint32_t offset) {
+// Calculate the offset of a observation within the ring buffer based on the index
+static uint32_t ring_buffer_offset(uint32_t index, uint32_t offset) {
     
     vmem_ring_driver_t * driver = (vmem_ring_driver_t *)(&vmem_images)->driver;
 
@@ -55,28 +21,23 @@ static uint32_t ring_buffer_offset(uint32_t payload_id, uint32_t offset) {
     uint32_t tail = driver->tail;
     uint32_t * offsets = (uint32_t *)driver->offsets;
 
-    uint32_t ring_buffer_index = payload_id < 0 // supports negative indexing from head
-        ? (head + payload_id + driver->entries) % driver->entries
-        : (tail + payload_id) % driver->entries;
+    uint32_t ring_buffer_index = (tail + index) % driver->entries;
 
-    uint32_t offset_of_payload = offsets[ring_buffer_index];
+    uint32_t offset_of_observation = offsets[ring_buffer_index];
 
-    return offset_of_payload + offset;
+    return offset_of_observation + offset;
 
 }
 
-// Find the size of a ring buffer element based on the payload_id
-static uint32_t element_size(uint32_t offset) {
+// Find the size of a observation within the ring buffer based on the index
+static uint32_t observation_size(uint32_t offset) {
     vmem_ring_driver_t * driver = (vmem_ring_driver_t *)(&vmem_images)->driver;
 
     uint32_t head = driver->head;
     uint32_t tail = driver->tail;
     uint32_t * offsets = (uint32_t *)driver->offsets;
 
-    int offset_int = (int)offset;
-    uint32_t read_from_index = offset_int < 0 // supports negative indexing from head
-        ? (head + offset_int + driver->entries) % driver->entries
-        : (tail + offset_int) % driver->entries;
+    uint32_t read_from_index = (tail + offset) % driver->entries;
     uint32_t read_to_index = (read_from_index + 1) % driver->entries;
 
     uint32_t read_from_offset = offsets[read_from_index];
@@ -95,42 +56,33 @@ static uint32_t element_size(uint32_t offset) {
     return len;
 }
 
-// Check if the payload_id provided results in a valid ring buffer index
-static int is_valid_index(uint32_t payload_id) {
+// Check if the index provided is a valid ring buffer index
+static int is_valid_index(uint32_t index) {
     vmem_ring_driver_t * driver = (vmem_ring_driver_t *)(&vmem_images)->driver;
 
-    if (payload_id >= driver->entries) return 0;
+    if (index >= driver->entries) return 0;
 
     uint32_t head = driver->head;
     uint32_t tail = driver->tail;
     uint32_t * offsets = (uint32_t *)driver->offsets;
 
-    int offset_int = (int)payload_id;
-
-    uint32_t index = offset_int < 0
-        ? (head + offset_int + driver->entries) % driver->entries
-        : (tail + offset_int) % driver->entries;
+    uint32_t ring_buffer_index = (tail + index) % driver->entries;
 
     int wraparound = driver->head < driver->tail;
 
     if (wraparound) {
-        return !((head <= index) && (index < tail));
+        return !((head <= ring_buffer_index) && (ring_buffer_index < tail));
     } else {
-        return (tail <= index) && (index < head);
+        return (tail <= ring_buffer_index) && (ring_buffer_index < head);
     }
 }
 
-static uint32_t payload_read(uint16_t payload_id, uint32_t offset, void *output, uint32_t size) {
-
-    // printf("payload read\n");
-
+static uint32_t observation_read(uint16_t index, uint32_t offset_within_observation, void *output, uint32_t size) {
 	vmem_ring_driver_t * driver = (vmem_ring_driver_t *)(&vmem_images)->driver;
     
-    uint32_t ring_offset = ring_buffer_offset(payload_id, offset);
+    uint32_t offset_within_ring_buffer = ring_buffer_offset(index, offset_within_observation);
 
-    // printf("Reading payload with payload_id %u, at offset %u, ring buffer offset %u, size %u\n", payload_id, offset, ring_offset, size);
-
-	(&vmem_images)->read(&vmem_images, ring_offset, output, size);
+	(&vmem_images)->read(&vmem_images, offset_within_ring_buffer, output, size);
 
     return size; // Assume that everything has been read, since the vmem api doesn't return any value to indicate how much data is read
 }
@@ -138,36 +90,116 @@ static uint32_t payload_read(uint16_t payload_id, uint32_t offset, void *output,
 /* This method is implemented to tell the DTP server which payload to use */
 bool get_payload_meta(dftp_payload_meta_t *meta, uint16_t payload_id) {
 
-    // printf("call to get_payload_meta");
-
     vmem_ring_driver_t * driver = (vmem_ring_driver_t *)(&vmem_images)->driver;
 
     uint32_t head = driver->head;
     uint32_t tail = driver->tail;
     uint32_t * offsets = (uint32_t *)driver->offsets;
 
-    // printf("images ring buffer information \n");
-    // printf("data_size is %u \n", driver->data_size);
-    // printf("number of entries is %u \n", driver->entries);
-    // printf("tail is at %u \n", tail);
-    // printf("head is at %u \n", head);
     int is_valid = is_valid_index(payload_id);
     if (!is_valid) {
-        printf("index out of ring buffer bounds \n");
         return false;
     }
-    uint32_t data_len = element_size(payload_id);
-    // printf("length of payload element is %u \n", data_len);
-
-    // printf("offsets are: \n");
-    // for (int i = 0; i<=driver->entries; i++) {
-    //     printf("    %u : %u \n", i, offsets[i]);
-    // }
+    uint32_t data_len = observation_size(payload_id);
 
     meta->size = data_len;
-    meta->read = payload_read;
-
-    // printf("returning\n");
+    meta->read = observation_read;
 
     return true;
+}
+
+// Get size of metadata for a specific observation
+static uint32_t observation_get_meta_size(uint16_t index) {
+    uint32_t meta_size;
+    uint8_t meta_size_buf[sizeof(meta_size)];
+    observation_read(index, 0, meta_size_buf, sizeof(meta_size)); // The first 4 bytes of each observation is the size of the metadata section
+    memcpy(&meta_size, (uint32_t *) meta_size_buf, sizeof(meta_size));
+    return meta_size;
+}
+
+// Get metadata of a specific observation
+static Metadata *observation_get_metadata(uint16_t index) {
+    uint32_t meta_size = observation_get_meta_size(index);
+    char meta_buf[meta_size];
+    observation_read(index, 4, meta_buf, meta_size);
+    Metadata *metadata = metadata__unpack(NULL, meta_size, meta_buf);
+    return metadata;
+}
+
+// Get metadata information about indeces
+static DTPMetadata *get_indeces_metadata() {
+
+    vmem_ring_driver_t * driver = (vmem_ring_driver_t *)(&vmem_images)->driver;
+
+    int observation_amount;
+    if (driver->head >= driver->tail) {
+        observation_amount = driver->head - driver->tail;
+    } else {
+        observation_amount = driver->entries - driver->tail + driver->head;
+    }
+
+    DTPMetadata *dtpmeta = malloc(sizeof(DTPMetadata));
+    dtpmetadata__init(dtpmeta);
+    dtpmeta->n_items = observation_amount;
+    dtpmeta->items = malloc(observation_amount * sizeof(DTPMetadataItem*));
+
+    for (int index = 0; index < observation_amount; index++) {
+        int size = observation_size(index);
+        Metadata *metadata = observation_get_metadata(index);
+        DTPMetadataItem *item = malloc(sizeof(DTPMetadataItem));
+        dtpmetadata_item__init(item);
+        item->payload_id = index;
+        item->size = size;
+        item->obid = 0;
+        dtpmeta->items[index] = item;
+    }
+
+    return dtpmeta;
+}
+
+// Server for serving indeces of ring buffer
+void dtp_indeces_server() {    
+    
+    static csp_socket_t sock = {0};
+    sock.opts = CSP_O_RDP;
+    csp_bind(&sock, 13);
+    csp_listen(&sock, 3);
+
+    csp_conn_t *conn;
+
+    while (1) {
+        if ((conn = csp_accept(&sock, 10000)) == NULL)
+        {
+            continue;
+        }
+
+        DTPMetadata *dtpmetadata = get_indeces_metadata();
+
+        uint32_t dtpmetadata_size = dtpmetadata__get_packed_size(dtpmetadata);
+
+        csp_packet_t * length_packet = csp_buffer_get(sizeof(uint32_t));
+        length_packet->length = sizeof(uint32_t);
+        memcpy(length_packet->data, &dtpmetadata_size, sizeof(uint32_t));
+        csp_send(conn, length_packet);
+
+        // Pack dtpmetadata struct into a buffer
+        uint8_t *dtpmetadata_packed = malloc(dtpmetadata_size);
+        dtpmetadata__pack(dtpmetadata, dtpmetadata_packed);        
+
+        // Send contents of buffer
+        int count = 0;
+        while((count < dtpmetadata_size) && csp_conn_is_active(conn)) {
+
+            // packet size should perhaps be a macro
+            csp_packet_t * packet = csp_buffer_get(196);
+            packet->length = dtpmetadata_size - count < 196 ? dtpmetadata_size - count : 196;
+            memcpy(packet->data, (void *) (dtpmetadata_packed + count), packet->length);
+
+            count += packet->length;
+            csp_send(conn, packet);
+        }
+
+        csp_close(conn);
+    }
+
 }
